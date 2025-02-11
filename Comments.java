7b811cb18a09fae32bb9dbcbcf184c72b54b1b1a
Miklos-vadasz <mvadasz@stu.socsd.org>
2025-02-11 10:26:26 -0500

    javae
    dc

---
+++ Comments.java
@@ -0,0 +1,16 @@
+/************************************
+*Project: Demo for Comments
+*Programmer: Miklos vadasz
+*Date: June, 2025
+*Program Name: Comments.java
+*************************************/
+
+public class Comments 
+{
+    public static void main(String[]args)
+    {
+     System.out.println("Hi");
+     System.out.println("LabOneA");
+    }
+}   
+
