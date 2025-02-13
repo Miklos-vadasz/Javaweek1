425626bc9c6bbff67cb465bfa4270287fccfed4e
Miklos-vadasz <mvadasz@stu.socsd.org>
2025-02-13 09:46:19 -0500

    eererejavan

--- ThreeExamples.java
+++ ThreeExamples.java
@@ -1,7 +1,9 @@
 public class ThreeExamples {
     public static void main(String[]args)
     {
-       System.out.println("\n\tHello\\\'");
+       System.out.println("Q\"yz89\"Ja4\"tp");
+       System.out.println("Place: \t1\t2\t3\t4\t5\t6\nDog#:\t76\t32\t18\t47\t11\t29");
+       System.out.println("Anderson\t3452\nBoyton\t        5344\nCarrington\t3677\nHouston\t        5988\nMcDougal\t5233\nRoberton\t3623\nStryker\t        5112");
 
 
     }
